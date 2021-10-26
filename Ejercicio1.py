a40d197de77fc083f271cc1a929544f6acfe50dc
SebastianBelalcazar <93206750+SebastianBelalcazar@users.noreply.github.com>
2021-10-26 09:27:24 -0500

    Add files via upload

---
+++ Ejercicio1.py
@@ -0,0 +1,22 @@
+# 2.Carolina desea comprar dos carros, uno para sus padres y otro para ella, para su sorpresa no encontró un sitio que comparara los distintos modelos y precios de su ciudad, así que va a investigar por sí misma.
+# Carolina preguntará cuantos concesionarios hay en la parte sur de Cali (cerca a su casa) y por cada concesionario preguntará cuantos modelos diferentes de carros tiene. Usted ha sido contratado por Carolina para ayudarle a resolver su problema. 
+# Construya una función en Python que reciba como parámetro la cantidad de concesionarios. Por cada concesionario el algoritmo debe preguntar el nombre del concesionario si el concesionario tiene un siguiente modelo. Por cada uno de los modelos el algoritmo debe preguntar el nombre del modelo, su precio, su consumo de gasolina en km por galón y su potencia. 
+# Finalmente, el algoritmo debe responder a las siguientes solicitudes: 
+# •	Carolina desea comprar el modelo que tenga el menor consumo de gasolina por km recorrido. Muestre en pantalla por cada concesionario, el modelo con menor consumo de gasolina y su precio. 
+# •	Para escoger el de sus padres, Carolina desea saber la potencia promedio de los carros de cada concesionario. Muestre por cada concesionario la potencia promedio de los carros. 
+# •	¿Cuántos modelos en todos los concesionarios tienen un precio menor de 40,000,000?
+
+def main():
+
+    
+
+
+
+
+
+
+
+
+
+
+main()
